b88bbc026249b50d28c2c7800b52b9d00544abd5
shubham.s <shubham.s@go-jek.com>
2018-02-03 12:39:40 +0530

    [Saxena] add middleman-livereload for hot reloading

--- config.rb
+++ config.rb
@@ -13,6 +13,9 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+# hot reloading of the page on code change
+activate :livereload
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
